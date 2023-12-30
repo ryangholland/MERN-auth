638330547390d26840539dcdae560b6e22ad62df
ryangholland <rghcode@gmail.com>
2023-12-30 18:54:26 -0500

    Set up React toastify

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,17 @@
 import { Outlet } from "react-router-dom";
+import { Container } from "react-bootstrap";
 import Header from "./components/Header";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const App = () => {
   return (
     <>
       <Header />
-      <Outlet />
+      <ToastContainer />
+      <Container>
+        <Outlet />
+      </Container>
     </>
   );
 };
